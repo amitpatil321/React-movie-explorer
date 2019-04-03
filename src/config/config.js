75da88ec1538c342174fa8320c9f5ab3e43f5c79
Amit <amitpatil321@gmail.com>
2019-04-04 01:21:28 +0530

    Add config for records limit

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,10 @@
 export const API_KEY = "08308f4c5d9eb1e0301d7a1887838bec"; 
 export const DEFAULT_POSTER = "/images/noposter.png"; 
 
+export const MOVIES_PER_PAGE = 18;
+export const CAST_PER_PAGE = 8;
+export const COMPANIES_PER_PAGE = 8;
+
 // Routes
 export const MOVIE_DETAILS_PAGE = "/movie/";
 export const PRODUCTION_COMPANY = "/company/";
