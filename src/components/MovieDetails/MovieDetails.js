1ef68a679c3592be5485495b65456bcabf95c5f7
Amit <amitpatil321@gmail.com>
2019-04-06 02:44:35 +0530

    Even more decomposing movie details component

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -18,7 +18,7 @@ class MovieDetails extends Component {
     state = {
         movie : null,
         error : null,
-        ignore : false // Whether to ignore to this error ?
+        ignore : false // Whether to print this error ?
     }
 
     componentDidMount(){
@@ -53,66 +53,86 @@ class MovieDetails extends Component {
     }
 
     render() {
-        let tags = "";
-        let companies = "";
-        let casts = "";
-
         // Handle error and show error message
         if(this.state.error != null && !this.state.ignore)
             return this.state.error;
 
         if(this.state.movie){
-            // Destructuring
-            let { title, name, backdrop_path, poster_path, tagline, overview } = this.state.movie;
-
-            // Check whether title OR name provided
-            title = (title) ? title : name;
-
-            // Set background image
-            let backgroundImage = "http://image.tmdb.org/t/p/original"+ backdrop_path;
-                document.getElementById("mainContent").style.backgroundImage = 'url("'+backgroundImage+'")';
-                
-                return (
-                        <div className="movieDetails">
-                            <Row gutter={16}>
-                                <Col xs={24} lg={7} className="moviePoster">                  <Fade>
-                                        <img src={"http://image.tmdb.org/t/p/w342"+poster_path} alt={title} width="294px"/>
-                                    </Fade>
-                                </Col>
-                                <Col xs={{span:24, offset : 0}} lg={16} offset={1}>
-                                    <Fade>
-                                        <Title className="movieName">
-                                            {title}
-                                        </Title>
-                                        <Title level={2} className="movieTagline">{tagline}</Title>
-                                        <Paragraph className="overview">
-                                            {overview}
-                                        </Paragraph>
-                                        <div className="tags">
-                                            <Tags movie = {this.state.movie} />
-                                        </div>
-                                    </Fade>
-                                </Col>
-                                
-                                <Col xs={24} lg={16} className="prodCompanies" type="flex">
-                                    <ProdCompanies movie = {this.state.movie} />
-                                </Col>  
-                                
-                                <Col xs={24} lg={16}>
-                                    <MovieMeta movie={this.state.movie}/>
-                                </Col>
-
-                            </Row>
-                            <Row>
-                                <Col span={24} className="cast">
-                                    <Cast movie={this.state.movie}/>
-                                </Col>
-                            </Row>
-                        </div>
-                );
+            return (
+                    <div className="movieDetails">
+                        <Row gutter={16}>
+                            <MovieInfo movie={this.state.movie} />
+                            <ProductionCompanies movie={this.state.movie} />
+                            <Meta movie={this.state.movie} />
+                        </Row>
+                        <Row>
+                            <MovieCast movie={this.state.movie} />
+                        </Row>
+                    </div>
+            );
         }else 
             return (<Empty />);
     }
+}
+
+const MovieInfo = (props) => {
+    if(props.movie){
+        // Destructuring
+        let { title, name, backdrop_path, poster_path, tagline, overview } = props.movie;
+
+        // Check whether title OR name provided
+        title = (title) ? title : name;
+
+        // Set background image
+        let backgroundImage = "http://image.tmdb.org/t/p/original"+ backdrop_path;
+            document.getElementById("mainContent").style.backgroundImage = 'url("'+backgroundImage+'")';
+        return (
+            <>
+                <Col xs={24} lg={7} className="moviePoster">                  <Fade>
+                        <img src={"http://image.tmdb.org/t/p/w342"+poster_path} alt={title} width="294px"/>
+                    </Fade>
+                </Col>
+                <Col xs={{span:24, offset : 0}} lg={16} offset={1}>
+                    <Fade>
+                        <Title className="movieName">
+                            {title}
+                        </Title>
+                        <Title level={2} className="movieTagline">{tagline}</Title>
+                        <Paragraph className="overview">
+                            {overview}
+                        </Paragraph>
+                        <div className="tags">
+                            <Tags movie = {props.movie} />
+                        </div>
+                    </Fade>
+                </Col>
+            </>       
+        );
+    }      
+}
+
+const ProductionCompanies = (props) => {
+    return (
+        <Col xs={24} lg={16} className="prodCompanies" type="flex">
+            <ProdCompanies movie = {props.movie} />
+        </Col>
+    );  
+}
+
+const Meta = (props) => {
+    return (
+        <Col xs={24} lg={16}>
+            <MovieMeta movie={props.movie}/>
+        </Col>
+    );
+}
+
+const MovieCast = (props) => {
+    return (
+        <Col span={24} className="cast">
+            <Cast movie={props.movie}/>
+        </Col>
+    );    
 }
 
 export default MovieDetails;
