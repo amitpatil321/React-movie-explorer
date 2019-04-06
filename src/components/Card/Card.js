20ab7a3e5eafc33e982dbfcefa2b37be93f8e79c
Amit <amitpatil321@gmail.com>
2019-04-06 21:29:47 +0530

    Code cleanup

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,19 +5,6 @@ import './Card.css';
 const { Meta } = Card;
 
 const MovieCard = props => {
-    // return (<Card
-    //     hoverable
-    //     bordered
-    //     style={{ width: 240 }}
-    //     cover={<img alt={props.title} src={ "http://image.tmdb.org/t/p/w342/"+props.poster } />}
-    //     className="eachcard"
-    // >
-    //     <Meta
-    //         title       = {props.title}
-    //         description = {<Rate allowHalf defaultValue={props.rating / 2} tooltips={props.rating} disabled />}  
-    //     />              
-    // </Card>);   
-
     return (
         <div className="content">
             <div className="content-overlay"></div>
