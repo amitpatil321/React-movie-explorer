37362dc529e5e04405e3274c9e525e24a7c026d3
Amit <amitpatil321@gmail.com>
2019-04-07 00:58:59 +0530

    Add genre page

---
+++ src/components/Genre/Genre.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+class Genre extends Component {
+    render() {
+        return (
+            <div>
+                This is genre page
+            </div>
+        );
+    }
+}
+
+export default Genre;
