23f4f30f9c36f5d7364b88766dddbf41cbd83bda
Amit <amitpatil321@gmail.com>
2019-04-11 01:49:29 +0530

    Code cleanup by moving inline css to css file

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,24 +2,29 @@ import React, { Component } from 'react';
 import { Menu, Layout } from 'antd';
 import { Link } from 'react-router-dom';
 import SearchMovies from '../SearchMovies/SearchMovies';
+
+import './Menu.css'
+
 const { Header } = Layout;
 
 const HeaderMenu = (props) => {
       return (
-        <Header style={{ position: 'fixed', zIndex: 1, width: '100%', paddingBottom: "70px"}}>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="home"><Link to="/">Home</Link></Menu.Item>
-            <Menu.Item key="search" disabled>
-              <SearchMovies />
-            </Menu.Item>
-          </Menu>
-        </Header>        
+        <div className="dummyHeader">
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="home"><Link to="/">Home</Link></Menu.Item>
+              <Menu.Item key="search" disabled>
+                <SearchMovies />
+              </Menu.Item>
+            </Menu>
+          </Header>
+        </div>        
       )
 }  
 
