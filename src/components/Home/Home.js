d88a2ca6850d8714c98c9be09a8b7b2a00443bdf
Amit <amitptil321@gmail.com>
2019-04-11 13:27:51 +0530

    Assigning individual mainContent holder for each page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,9 +11,9 @@ class Home extends Component {
 
     render() {
         return (
-            <>
+            <div className="mainContent" id="mainContent">
                 <ListMovies type="trending"/>
-            </>    
+            </div>    
         );
     }
 }
