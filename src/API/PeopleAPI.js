0fcf1799f12ec9e9d6d8a6d23e9e8f4e21ed1ad5
Amit <amitptil321@gmail.com>
2019-04-12 21:27:20 +0530

    Fetching only movie credits

--- src/API/PeopleAPI.js
+++ src/API/PeopleAPI.js
@@ -6,7 +6,7 @@ var People = {};
 
 // Get basic profile details
 People.profile = (id) => {
-    return MoviesAxios.get(CONFIG.API_BASE_URL+"/person/"+id).then(response => {
+    return MoviesAxios.get(CONFIG.API_BASE_URL+"/person/"+id+"?append_to_response=movie_credits,external_ids,images").then(response => {
         return response.data;
     })
     .catch((error) => {
