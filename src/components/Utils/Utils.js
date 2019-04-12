f0b1a60b22ffad564b126b867d4a6706234d1767
Amit <amitptil321@gmail.com>
2019-04-12 21:23:55 +0530

    Add getAge function

--- src/components/Utils/Utils.js
+++ src/components/Utils/Utils.js
@@ -7,3 +7,10 @@ export function makeUrl(str) {
 export function removeBg(){
     document.getElementById("layout").style.backgroundImage = '';
 }
+
+export function getAge(birthdate){
+    var birthdate = new Date(birthdate);
+    var cur = new Date();
+    var diff = cur-birthdate;
+    return Math.floor(diff/31536000000);    
+}
