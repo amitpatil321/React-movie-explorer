9f58d15e78b31d1c177d3c3fce7efb9aacc78236
Amit <amitptil321@gmail.com>
2019-04-30 16:33:38 +0530

    Linked logo to home page

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Layout } from 'antd';
+import { Layout } from 'antd';
 import { Link } from 'react-router-dom';
 import SearchMovies from '../SearchMovies/SearchMovies';
 
@@ -12,7 +12,9 @@ const HeaderMenu = (props) => {
         <div className="dummyHeader">
           <Header>
             <div className="logo" >
-              <img alt="Logo" src="/images/logo.png" />
+              <Link to="/">
+                <img alt="Logo" src="/images/logo.png" />
+              </Link>
             </div>
             <SearchMovies />
           </Header>
